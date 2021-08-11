6fe5dc62043aecdf6e39a8173624d05fedc78745
Pogula Pavani <pogulapavani@gmail.com>
2021-08-11 07:30:31 +0530

    add comment

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Item from "./components/Item";
 import Search from "./components/Search";
 import NotFound from "./components/NotFound";
 
+//redux gallery 
 class App extends Component {
   // Prevent page reload, clear input, set URL and push history on submit
   handleSubmit = (e, history, searchInput) => {
